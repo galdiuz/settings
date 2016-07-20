8c2c7603a91a9aed19d5ae79c2fcc5a4f1919c8c
Andreas Lundkvist <github@galdiuz.com>
2016-07-20 22:35:26 +0200

    Added git settings

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
 . installers/grub-customizer.sh
 . installers/oh-my-zsh.sh
 . installers/vim.sh
+. installers/git.sh
---
+++ installers/git.sh
@@ -0,0 +1,4 @@
+git config --global core.editor vim
+git config --global user.email "github@galdiuz.com"
+git config --global user.name "Andreas Lundkvist"
+git config --global push.default simple
