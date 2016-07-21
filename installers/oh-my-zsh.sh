dc6b89298695c73a29c96a8c4217f3186b49497e
Andreas Lundkvist <andreas.lundkvist@webbhuset.se>
2016-07-21 08:46:32 +0200

    Added zshrc

--- installers/oh-my-zsh.sh
+++ installers/oh-my-zsh.sh
@@ -1,2 +1,6 @@
 sudo apt-get install zsh -y
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+rm -f ~/.zshrc
+CURRENT_DIR="$(pwd)"
+ln -s $CURRENT_DIR/zshrc ~/.zshrc
