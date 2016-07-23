9397bc376330baf79fbe2918fe8f9d6b52c56098
Andreas Lundkvist <github@galdiuz.com>
2016-07-23 17:57:48 +0200

    Added chrome gnome extension

--- installers/chrome.sh
+++ installers/chrome.sh
@@ -1,4 +1,5 @@
 wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
 sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
+sudo add-apt-repository -y ppa:ne0sight/chrome-gnome-shell
 sudo apt-get update
-sudo apt-get install google-chrome-stable -y
+sudo apt-get install -y google-chrome-stable chrome-gnome-shell
